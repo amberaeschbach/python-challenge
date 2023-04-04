27e39b564fe4ac620af2e2994bd1a8e066c6866e
Amber <araeschbach@ucdavis.edu>
2023-04-03 23:54:11 -0700

    finished

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,31 +2,54 @@ import os
 import csv
 
 DATA_FILENAME = os.path.join(os.path.dirname(__file__), r'Resources\budget_data.csv')
+OUTPUT_FILENAME = os.path.join(os.path.dirname(__file__), r'Analysis\output.txt')
 
 def main():
     with open(DATA_FILENAME, "r") as file:
         reader = csv.DictReader(file)
-        months = 0
-        net_value = 0
+        changes = []
         greatest_increase = {}
         greatest_decrease = {}
+
+        previous_profit_losses = 0
+
+        line = next(reader)
+        months = 1
+        value = int(line['Profit/Losses'])
+        previous_profit_losses = value
+        net_value = value
 
         for line in reader:
             months += 1
             value = int(line['Profit/Losses'])
+
+            change = -(previous_profit_losses - value)
+            previous_profit_losses = value
+            changes.append(change)
+
             net_value += value
 
-            if not greatest_increase or value > int(greatest_increase['Profit/Losses']):
-                greatest_increase = line
+            if not greatest_increase or change > int(greatest_increase['Increase']):
+                greatest_increase = {"Date": line['Date'], "Increase": change}
                 
-            if not greatest_decrease or value < int(greatest_decrease['Profit/Losses']):
-                greatest_decrease = line
+            if not greatest_decrease or change < int(greatest_decrease['Decrease']):
+                greatest_decrease = {"Date": line['Date'], "Decrease": change}
 
         print(f"Total Months: {months}")
         print(f"Total: ${net_value}")
-        print(f"Average change: ${int(net_value/months)}")
-        print(f"Greatest Increase in Profits: {greatest_increase['Date']} (${greatest_increase['Profit/Losses']})")
-        print(f"Greatest Descrease in Profits: {greatest_decrease['Date']} (${greatest_decrease['Profit/Losses']})")
+        print(f"Average change: ${sum(changes)/len(changes):.2f}")
+        print(f"Greatest Increase in Profits: {greatest_increase['Date']} (${greatest_increase['Increase']})")
+        print(f"Greatest Descrease in Profits: {greatest_decrease['Date']} (${greatest_decrease['Decrease']})")
+
+        # Create output dir if it doesn't exit
+        os.makedirs(os.path.dirname(OUTPUT_FILENAME), exist_ok=True)
+        # Save to file in a 'fancy' way
+        with open(OUTPUT_FILENAME, "w") as output:
+            print(f"Total Months: {months}", file=output)
+            print(f"Total: ${net_value}", file=output)
+            print(f"Average change: ${sum(changes)/len(changes):.2f}", file=output)
+            print(f"Greatest Increase in Profits: {greatest_increase['Date']} (${greatest_increase['Increase']})", file=output)
+            print(f"Greatest Descrease in Profits: {greatest_decrease['Date']} (${greatest_decrease['Decrease']})", file=output)
 
 if __name__ == "__main__":
     main()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,18 +1,24 @@
+import os
 import csv
 
+DATA_FILENAME = os.path.join(os.path.dirname(__file__), r"Resources\election_data.csv")
+OUTPUT_FILENAME = os.path.join(os.path.dirname(__file__), r'Analysis\output.txt')
+
 def main():
-    with open(r"Resources\election_data.csv", "r") as file:
-        reader = csv.DictReader(file)
+    with open(DATA_FILENAME, "r") as file:
+        reader = csv.reader(file)
         votes = 0
         candidates = {}
+
+        header = next(reader)
 
         for line in reader:
             votes += 1
 
-            if line['Candidate'] not in candidates:
-                candidates[line['Candidate']] = 1
+            if line[2] not in candidates:
+                candidates[line[2]] = 1
             else:
-                candidates[line['Candidate']] += 1
+                candidates[line[2]] += 1
 
         
         print(f"Election Results")
@@ -26,6 +32,20 @@ def main():
         print(f"------------------------------")
         print(f"*** Winner: {sorted(candidates, key=candidates.get, reverse=True)[0]} ***")
         print(f"------------------------------")
+
+        os.makedirs(os.path.dirname(OUTPUT_FILENAME), exist_ok=True)
+        with open(OUTPUT_FILENAME, "w") as output:
+            output.write(f"Election Results\n")
+            output.write(f"------------------------------\n")
+            output.write(f"Total Votes: {votes}\n")
+            output.write(f"------------------------------\n")
+
+            for name, c_votes in candidates.items():
+                output.write(f"{name}: {(c_votes/votes*100):.3f}% ({c_votes})\n")
+
+            output.write(f"------------------------------\n")
+            output.write(f"*** Winner: {sorted(candidates, key=candidates.get, reverse=True)[0]} ***\n")
+            output.write(f"------------------------------\n")
 
 if __name__ == "__main__":
     main()
