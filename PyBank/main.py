02fa96b5615227acc8112f0fddf0e722be790b7c
Amber <araeschbach@ucdavis.edu>
2023-04-04 00:06:51 -0700

    FINAL

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,12 @@
+#import the csv file with the data we want
 import os
 import csv
 
+#set the path for the file and the output file
 DATA_FILENAME = os.path.join(os.path.dirname(__file__), r'Resources\budget_data.csv')
 OUTPUT_FILENAME = os.path.join(os.path.dirname(__file__), r'Analysis\output.txt')
 
+#creating variables and reading the csv file
 def main():
     with open(DATA_FILENAME, "r") as file:
         reader = csv.DictReader(file)
@@ -19,6 +22,7 @@ def main():
         previous_profit_losses = value
         net_value = value
 
+#using a for loop to find the greatest increase/decrease between the months
         for line in reader:
             months += 1
             value = int(line['Profit/Losses'])
@@ -34,16 +38,16 @@ def main():
                 
             if not greatest_decrease or change < int(greatest_decrease['Decrease']):
                 greatest_decrease = {"Date": line['Date'], "Decrease": change}
-
+#printing the results in the terminal
         print(f"Total Months: {months}")
         print(f"Total: ${net_value}")
         print(f"Average change: ${sum(changes)/len(changes):.2f}")
         print(f"Greatest Increase in Profits: {greatest_increase['Date']} (${greatest_increase['Increase']})")
         print(f"Greatest Descrease in Profits: {greatest_decrease['Date']} (${greatest_decrease['Decrease']})")
 
-        # Create output dir if it doesn't exit
+        # create output dir if it doesn't exit for a text file with the results
         os.makedirs(os.path.dirname(OUTPUT_FILENAME), exist_ok=True)
-        # Save to file in a 'fancy' way
+        # save to file in a way
         with open(OUTPUT_FILENAME, "w") as output:
             print(f"Total Months: {months}", file=output)
             print(f"Total: ${net_value}", file=output)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,6 +1,7 @@
+#import csv for the data we need
 import os
 import csv
-
+#setting paths for the file and the new output file
 DATA_FILENAME = os.path.join(os.path.dirname(__file__), r"Resources\election_data.csv")
 OUTPUT_FILENAME = os.path.join(os.path.dirname(__file__), r'Analysis\output.txt')
 
@@ -11,7 +12,7 @@ def main():
         candidates = {}
 
         header = next(reader)
-
+#using a for loop to find the candidates that received votes
         for line in reader:
             votes += 1
 
@@ -25,14 +26,14 @@ def main():
         print(f"------------------------------")
         print(f"Total Votes: {votes}")
         print(f"------------------------------")
-
+#using a for loop to find the percentage of votes for each candidate and then finding a winner
         for name, c_votes in candidates.items():
             print(f"{name}: {(c_votes/votes*100):.3f}% ({c_votes})")
 
         print(f"------------------------------")
         print(f"*** Winner: {sorted(candidates, key=candidates.get, reverse=True)[0]} ***")
         print(f"------------------------------")
-
+#creating an output for a text file that will print the results
         os.makedirs(os.path.dirname(OUTPUT_FILENAME), exist_ok=True)
         with open(OUTPUT_FILENAME, "w") as output:
             output.write(f"Election Results\n")
